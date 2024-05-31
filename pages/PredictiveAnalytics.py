dbacf792a65e52a6b78a075b9880f6538819ad3e
Ruben Giro <Ruben.Giro@gmail.com>
2024-05-31 00:36:12 -0400

    j

--- pages/PredictiveAnalytics.py
+++ pages/PredictiveAnalytics.py
@@ -3,7 +3,7 @@ import dash_bootstrap_components as dbc
 from dash import dcc, Dash, callback
 import plotly.express as px
 from dash import Input, Output, dcc, html
-from graph import *
+from pages.graph import *
 
 dash.register_page(__name__, path='/PredictiveAnalytics')
 DARK_COLOR = 'rgba(0, 0, 0, 0)'
@@ -15,27 +15,27 @@ per_list_g1 = period_list(per_currQ, 8)
 per_list_g2 = period_list(per_currQ, 5)
 
 df1 = dfq[dfq.index.isin(per_list_g1)]
-g1 = create_results_graph(df1, 'Revenue').show()
-g11 = create_results_graph(df1, 'End_Count').show()
+g1 = create_results_graph(df1, 'Revenue')
+g11 = create_results_graph(df1, 'End_Count')
 
-g2 = cust_detail.groupby('hotspot')[per_list_g2].sum()
-create_byplan_graph(g2).show()
+df2 = cust_detail.groupby('hotspot')[per_list_g2].sum()
+g2 = create_byplan_graph(df2)
 
-g3 = cust_detail[cust_detail[per_currQ]!=0].groupby(['hotspot'
+df3 = cust_detail[cust_detail[per_currQ]!=0].groupby(['hotspot'
         , 'Household','duration'])[per_currQ].count().reset_index()
-create_customer_profile_graph(g3).show()
+g3 = create_customer_profile_graph(df3)
 
-g4 = compare_periods(dfq, per_currQ, per_currQ-4)
-create_compare_graph(g4).show()
+df4 = compare_periods(dfq, per_currQ, per_currQ-4)
+g4 = create_compare_graph(df4)
 
-g5 = df
-create_month_add_graph(g5, 'Jun', 1200).show()
+g5 = create_month_add_graph(df, 'Jun', 1200)
 
 
 dcc_g1 = dcc.Graph(figure=g1, id='g1')
 dcc_g11 = dcc.Graph(figure=g11, id='g11')
 
 def create_card(header="Marketing", text ="Main text", para = "bottomtext"):
+    global id_counter
     header_id = "id" + str(id_counter)
     text_id = "id" + str(id_counter)
     para_id = "id" + str(id_counter)
